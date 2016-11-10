bea4bdabf59728db8cfca217397da8c80940a287
Alexander Glassford <a.b.glassford@gmail.com>
2016-11-10 16:06:50 -0700

    deploy

--- config.php
+++ config.php
@@ -1,16 +1,29 @@
 <?php
 
-return  [
-  'database' => [
-    'name' => 'unityAccelerator',
-    'username' => 'root',
-    'password' => 'root',
-    'connection' => 'mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
-    'options' => [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ]
-  ]
-];
+// return  [
+//   'database' => [
+//     'name' => 'unityAccelerator',
+//     'username' => 'root',
+//     'password' => 'root',
+//     'connection' => 'mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
+//     'options' => [
+//       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//     ]
+//   ]
+// ];
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$config = array(
+    'host' => $server ,
+    'user' => $username ,
+    'pw' => $password,
+    'db' => $db
+);
 
 
 //test stuff
