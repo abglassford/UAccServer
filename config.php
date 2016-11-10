f6e10e1e6ea3d71348c77197230d8d311a6ac188
Alexander Glassford <a.b.glassford@gmail.com>
2016-11-10 16:16:02 -0700

    Add Procfile

--- config.php
+++ config.php
@@ -1,29 +1,28 @@
 <?php
 
-// return  [
-//   'database' => [
-//     'name' => 'unityAccelerator',
-//     'username' => 'root',
-//     'password' => 'root',
-//     'connection' => 'mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
-//     'options' => [
-//       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-//     ]
-//   ]
-// ];
+return  [
+  'database' => [
+    'name' => 'unityAccelerator',
+    'username' => 'root',
+    'password' => 'root',
+    'connection' => 'mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]
+  ]
+];
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+//for heroku deployment
 
-$config = array(
-    'host' => $server ,
-    'user' => $username ,
-    'pw' => $password,
-    'db' => $db
-);
-
-
-//test stuff
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $server = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $db = substr($url["path"], 1);
+//
+// $config = array(
+//     'host' => $server ,
+//     'user' => $username ,
+//     'pw' => $password,
+//     'db' => $db
+// );
