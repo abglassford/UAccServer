76d53584efef498813441c471f6d810632dfb4b7
Alexander Glassford <a.b.glassford@gmail.com>
2016-11-11 11:17:26 -0700

    fix delete route

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
- header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 use \Psr\Http\Message\ServerRequestInterface as req;
 use \Psr\Http\Message\ResponseInterface as res;
